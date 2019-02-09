e8fc3e37111afd3c8c113a6c7cda7032ba406b5e
raiResham <42313064+raiResham@users.noreply.github.com>
2019-02-09 20:41:28 +0545

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,5 @@ public class Main {
 	public static void main(String args[]) {
 		Solver s = new Solver();
 		s.start();
-		
-		
-//		System.out.print("fuse.ai ");	 
-		//	\u000d	
-//		System.out.print("hack.ai");
-//	String a = "UURULBBBRUUULLU";
-//	System.out.print(a.length());
 	}
 }
